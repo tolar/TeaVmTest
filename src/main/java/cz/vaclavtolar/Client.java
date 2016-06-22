13cfc1d07bc3bc76a72b810a8a24a7a9e3a1c751
Vaclav Tolar <vaclav.tolar@gmail.com>
2016-06-22 22:41:37 +0200

    Initial commit

---
+++ src/main/java/cz/vaclavtolar/Client.java
@@ -0,0 +1,52 @@
+package cz.vaclavtolar;
+
+import org.teavm.jso.ajax.XMLHttpRequest;
+import org.teavm.jso.browser.Window;
+import org.teavm.jso.dom.events.EventListener;
+import org.teavm.jso.dom.events.MouseEvent;
+import org.teavm.jso.dom.html.HTMLButtonElement;
+import org.teavm.jso.dom.html.HTMLDocument;
+import org.teavm.jso.dom.html.HTMLElement;
+
+public class Client {
+
+    private static HTMLDocument document = Window.current().getDocument();
+    private static HTMLButtonElement helloButton = document.getElementById("hello-button").cast();
+    private static HTMLElement responsePanel = document.getElementById("response-panel");
+    private static HTMLElement thinkingPanel = document.getElementById("thinking-panel");
+
+    private Client() {
+    }
+
+    public static void main(String[] args) {
+        helloButton.listenClick(new EventListener<MouseEvent>() {
+            @Override
+            public void handleEvent(MouseEvent evt) {
+                sayHello();
+            }
+        });
+    }
+
+    private static void sayHello() {
+
+
+        helloButton.setDisabled(true);
+        thinkingPanel.getStyle().setProperty("display", "");
+        final XMLHttpRequest xhr = XMLHttpRequest.create();
+        xhr.onComplete(new Runnable() {
+            @Override
+            public void run() {
+                responsePanel.appendChild(document.createElement("div").withText(xhr.getResponseText()));
+                helloButton.setDisabled(false);
+                thinkingPanel.getStyle().setProperty("display", "none");
+            }
+        });
+        xhr.open("GET", "hello");
+        xhr.send();
+    }
+
+
+
+
+}
+
---
+++ src/main/java/cz/vaclavtolar/Files.java
@@ -0,0 +1,41 @@
+package cz.vaclavtolar;
+
+/**
+ * Created by vasek on 20. 6. 2016.
+ */
+public class Files {
+
+    public static final String PDF = "%PDF-1.4\n" +
+            "%áéëÓ\n" +
+            "2 0 obj\n" +
+            "<</Type /Catalog\n" +
+            "/Pages 1 0 R\n" +
+            ">>\n" +
+            "endobj\n" +
+            "3 0 obj\n" +
+            "<</Type /Page\n" +
+            "/Parent 1 0 R\n" +
+            "/Resources <</ExtGState <</G0 4 0 R\n" +
+            ">>\n" +
+            "/XObject <</X0 5 0 R\n" +
+            ">>\n" +
+            "/Font <</F0 6 0 R\n" +
+            "/F1 7 0 R\n" +
+            "/F2 8 0 R\n" +
+            ">>\n" +
+            "/ProcSet [/PDF /Text /ImageB /ImageC /ImageI]\n" +
+            ">>\n" +
+            "/MediaBox [0 0 596 843]\n" +
+            "/Contents 9 0 R\n" +
+            ">>\n" +
+            "endobj\n" +
+            "9 0 obj\n" +
+            "<</Filter /FlateDecode\n" +
+            "/Length 2617\n" +
+            ">> stream\n" +
+            "xœ¥[ÙŽdG\u0011}ï¯¨\u001F \u009D±ä&!$ÆÓãgÐHðŽ°\u0011b@˜ÿ—È¬^nÄ‰Îè\u0011¨m¹Ü§#2ö%ï\u00ADò8J™·²~~S^?\u000F•Û_¾=üûaÿzJyd\u0012½‘\u0090>ö¡ãöë_\u001FþtûçÂé‘u´;µù¸Hé¶\u007FþøÓíùÃ¯¿<üðS¹ýòŸ;Ç®õ~Äbóóý\u0017eÿåó‡õ—Ÿ¾>üðe\tqûúóâs—ŒnÜn,·¯ß\u001E~[\n" +
+            "éïn_ÿ~aR\fö{ÀÔ`\u0004˜åÙ=¦åL§|–E-ÏvÇž¾>üaý|dÍÖ\u001F•u›¿õö¸l4–€\u008F\\fû\u000E{¿X÷}ÓõñX/©\u0018$æ!\u0016Ö'„›…kGØ1×\u001F\u0001žÅQO„ýÙ(Úì\u0016þò„~'Ç\u001CÝ[ª…{ˆ\u001AÇ¼\bÂÓ1G˜œä•\u0011vÌk\u0005˜KfTa¶p›\b;—è\u0017„Gª˜¸³ùG„ÝÙÒ\u0010vŠ1J®ò\u000EõwfÀ\n" +
+            "ê•3\u008FÒºÞÚÔGæ¦/9Ðÿß\u001Ch·±þ}1Ø§P?.0ú\"\u0003Õ‚hèaÁ\u0016jÈ\u0005Òg";
+
+    public static final String PKCS12 = "";
+}
